1d9084870593811fce20a4e946ff85347200555a
moaible <moai.motodera@gmail.com>
2020-10-02 15:30:42 +0000

    Passed testcase

--- Tests/swift-cloud-kitTests/swift_cloud_kitTests.swift
+++ Tests/swift-cloud-kitTests/swift_cloud_kitTests.swift
@@ -26,7 +26,7 @@ final class swift_cloud_kitTests: XCTestCase {
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)
 
-        XCTAssertEqual(output, "Hello, world!\n")
+        XCTAssertEqual(output, "Hello, world\n")
     }
 
     /// Returns path to the built products directory.
